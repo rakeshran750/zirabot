cbc80cf5305e8bd371954710fece2622af2f48fc
Rakesh Ran <143284882+rakeshran750@users.noreply.github.com>
2025-12-16 12:13:54 +0000

    fix naming

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ChatUI from './components/Chatui'
+import ChatUI from './components/ChatUI.jsx'
 
 
 
